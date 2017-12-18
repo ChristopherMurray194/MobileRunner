cd28f01d7fab75ed240ed7901be148c00e9c9783
Christopher Murray <chris.murray194@hotmail.co.uk>
2017-12-18 19:01:09 +0000

    Player added with follow camera.

---
+++ Assets/Scripts/Camera/MainCamera.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCamera : MonoBehaviour
+{
+    /// <summary> Camera follow movement speed </summary>
+    public float followSpeed = 5f;
+    /// <summary> The target for the camera to follow </summary>
+    public Transform target;
+    /// <summary> Distance to maintain from the target </summary>
+    Vector3 distance;
+
+    Camera cam;
+    
+	void Start ()
+    {
+        distance = transform.position - target.position;
+        cam = GetComponent<Camera>();
+	}
+	
+	void Update ()
+    {
+        FollowPlayer();
+	}
+
+    void FollowPlayer()
+    {
+        // Move with the player
+        Vector3 posToTarget = target.position + distance;
+        transform.position = Vector3.Lerp(transform.position, posToTarget, followSpeed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    /// <summary> Movement speed of the player </summary>
+    public float movementSpeed = 15f;
+    
+	void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+        Move();
+	}
+
+    void Move()
+    {
+        Vector3 pos = transform.position;
+        pos.z += movementSpeed * Time.deltaTime;
+        transform.position = pos;
+    }
+}
