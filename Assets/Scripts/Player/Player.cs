fdf48ba946e11aaf7b7de5aeba45914bde5373af
Christopher Murray <chris.murray194@hotmail.co.uk>
2017-12-18 19:22:58 +0000

    Player can move left and right across lanes.

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -6,6 +6,9 @@ public class Player : MonoBehaviour
 {
     /// <summary> Movement speed of the player </summary>
     public float movementSpeed = 15f;
+
+    /// <summary> The increment at which the player moves left and right </summary>
+    float moveIncrement = 7.5f;
     
 	void Start ()
     {
@@ -14,13 +17,51 @@ public class Player : MonoBehaviour
 	
 	void Update ()
     {
-        Move();
-	}
+        MoveForward();
 
-    void Move()
+        if(Input.GetKeyDown(KeyCode.A))
+            MoveLeft();
+
+        if (Input.GetKeyDown(KeyCode.D))
+            MoveRight();
+    }
+
+    void MoveForward()
     {
         Vector3 pos = transform.position;
         pos.z += movementSpeed * Time.deltaTime;
+        transform.position = pos;
+    }
+
+    void MoveLeft()
+    {
+        Vector3 pos = transform.position;
+        // Ensure we do not go off the track
+        if(!(pos.x - moveIncrement < -26.25f))
+        {
+            pos.x -= moveIncrement;
+        }
+        else
+        {
+            // Notify player they cannot move any further left
+        }
+
+        transform.position = pos;
+    }
+
+    void MoveRight()
+    {
+        Vector3 pos = transform.position;
+        // Ensure we do not go off the track
+        if (!(pos.x + moveIncrement > -3.25f))
+        {
+            pos.x += moveIncrement;
+        }
+        else
+        {
+            // Notify player they cannot move any further right
+        }
+
         transform.position = pos;
     }
 }
